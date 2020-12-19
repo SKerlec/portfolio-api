aaf09ee91f4a5dbe7936f5e6b6c115add62852a2
Sebastian Holmes <SKerlec7693@gmail.com>
2020-12-18 17:06:38 -0800

    pushing all progress to repo

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const sendGrid = require('@sendgrid/mail');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(cors());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.get('/api', (req, res, next) => {
+    res.send('API status: running')
+});
+
+const apiKey = process.env.SENDGRID_API_KEY;
+
+app.post('/api/email', (req, res, next) => {
+    sendGrid.setApiKey(apiKey);
+    const msg = {
+        to: 'sholmes.dev@gmail.com',
+        from: 'SKerlec7693@gmail.com',
+        subject: 'Portfolio Contact from: ' + req.body.name,
+        text: 'Email: ' + req.body.email + ' ' + 'Message: ' + '"' + req.body.message + '"'
+    }
+    sendGrid.send(msg)
+        .then(result => {
+            res.status(200).json({
+                success: true
+            });
+        })
+        .catch(err => {
+            console.log('error: ', err);
+            res.status(401).json({
+                success: false
+            });
+        });
+});
+
+
+
+app.listen(3030, '0.0.0.0');
